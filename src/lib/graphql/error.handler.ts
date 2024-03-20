c3f893abd8e35d44e21717c505e26bfe7f2df9c9
ask-nugey <mischief.moi@gmail.com>
2024-03-21 04:21:47 +0900

    ✨ feat: エラーハンドリングの設定

---
+++ src/lib/graphql/error.handler.ts
@@ -0,0 +1,31 @@
+import { Logger } from "@/lib/graphql/logger";
+import { GraphQLError } from "graphql";
+
+// TODO: コードの意味を理解する
+export class GqlErrorHandler {
+  public static checkError = (
+    methodName: string,
+    errors: readonly GraphQLError[] | undefined
+  ): void => {
+    if (typeof errors === "undefined") return;
+    if (process.env.NODE_ENV !== "production") console.log(errors);
+
+    for (const error of errors) {
+      let msg = "";
+
+      if (typeof error.extensions?.path !== "undefined")
+        msg += "path: " + error.extensions.path + "\n";
+      if (typeof error.extensions?.code !== "undefined")
+        msg += "code: " + error.extensions.code + "\n";
+      if (typeof error.extensions?.problems !== "undefined")
+        msg += "problems: " + error.extensions.problems + "\n";
+      if (typeof error.message !== "undefined")
+        msg += "message: " + error.message + "\n";
+      if (msg === "") msg = String(error);
+
+      Logger.error(methodName, "Something graphql error...", msg);
+    }
+
+    throw new Error(`[${methodName}] Something graphql error...`);
+  };
+}
---
+++ src/lib/graphql/logger/index.ts
@@ -0,0 +1,49 @@
+// TODO: コードの意味を理解する
+type LoggerArgsType = {
+  status: "INFO" | "DEBUG" | "WARNING" | "ERROR";
+  path: string;
+  message: string;
+  value: unknown;
+};
+
+const commonLogger = (args: LoggerArgsType): void => {
+  const { path, message, value, status } = args;
+  console.log("===============================");
+  console.log(`${status}: [${path}]`);
+  console.log(`message: ${message}`);
+  console.log(JSON.stringify(value, null, 4));
+  console.log("===============================");
+};
+
+export class Logger {
+  public static debug = (message: string, value: unknown): void => {
+    if (process.env.NODE_ENV === "production") return;
+    commonLogger({ status: "DEBUG", path: "*", message, value });
+  };
+
+  public static info = (
+    path: string,
+    message: string,
+    value: unknown
+  ): void => {
+    commonLogger({ status: "INFO", path, message, value });
+  };
+
+  public static warning = (
+    path: string,
+    message: string,
+    value: unknown
+  ): void => {
+    commonLogger({ status: "WARNING", path, message, value });
+  };
+
+  public static error = (
+    path: string,
+    message: string,
+    value: unknown
+  ): void => {
+    commonLogger({ status: "ERROR", path, message, value });
+
+    if (typeof value === "undefined") return;
+  };
+}
