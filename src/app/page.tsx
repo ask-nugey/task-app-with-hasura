06cc19d67c72a7574e222e887e952c856b3dba43
ask-nugey <mischief.moi@gmail.com>
2024-03-20 21:36:56 +0900

    ✨ feat: shadcn/ui のセットアップ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/app/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <p className="text-orange-500 font-bold text-5xl">Hello World</p>
+      <Button className="my-5">Click</Button>
     </main>
   );
 }
